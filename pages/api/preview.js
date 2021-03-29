4fa8e2e6233e7d30e4681dbdc8a59961a43f788e
Rudo Hengst <rudohengst@gmail.com>
2021-03-28 20:50:27 -0400

    prepend access tokens with NEXT_PUBLIC

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -3,7 +3,7 @@ import { getPreviewPostBySlug } from "../../lib/api";
 export default async function preview(req, res) {
   const { secret, slug } = req.query;
 
-  if (secret !== process.env._CONTENTFUL_PREVIEW_SECRET || !slug) {
+  if (secret !== process.env._NEXT_PUBLIC_CONTENTFUL_PREVIEW_SECRET || !slug) {
     return res.status(401).json({ message: "Invalid token" });
   }
 
